855f6ab9690f5824a372e2ceaf27c46ac8efca57
JoanneIPTI <joannestephany@hotmail.com>
2024-04-01 14:01:38 -0300

    Add: logo da prefeitura
    Adicionada somente no front-end para não haver alterações no back por hora, em caso de nova cidade, criar nova task

--- src/Archives/Foward/archives.js
+++ src/Archives/Foward/archives.js
@@ -8,7 +8,7 @@ import { useFetchFamilyReferedId } from '../../sdk/FamilyRefered/request';
 import { useFetchOneTechnician, useFetchOneTechnicianPsico } from '../../sdk/Technician/EditTechnician/request';
 import { useFetchOneFowardByForwarding} from '../../sdk/FOUIForwarding/requests';
 import { Column, Row } from '../../CrasUi/styles/styles';
-
+import LogoNSLourdes from "../../assets/images/logo-prefeitura-nslourdes.png";
 
 // Estilos globais
 const GlobalStyle = createGlobalStyle`
@@ -120,6 +120,7 @@ const Document = ({ visibleEdit }) => {
       <GlobalStyle />
       <EncaminhamentoContainer>
         <HeaderContainer>
+          <img src={LogoNSLourdes} alt="Logo da Prefeitura de Nossa Senhora de Lourdes"/>
           <p>PREFEITURA MUNICIPAL DE {unityAttendance?.edcenso_city.name}</p>
           <p>SECRETARIA MUNICIPAL DE ASSISTÊNCIA SOCIAL</p>
           {unityAttendance?.type === 'CRAS' ? (
