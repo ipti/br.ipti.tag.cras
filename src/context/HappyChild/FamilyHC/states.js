8e2052ccf2522cd2ee039f312d1fb4e4dbbcf01f
JoanneIPTI <joannestephany@hotmail.com>
2024-06-05 16:21:28 -0300

    wip: happy child context nd states

--- src/context/HappyChild/FamilyHC/states.js
+++ src/context/HappyChild/FamilyHC/states.js
@@ -1,16 +1,4 @@
 import { useEffect, useState } from "react";
-<<<<<<< HEAD
-import { useFetchAllFamilyHC } from "../../../sdk/HappyChild/FamilyHC/request";
-
-export const FamilyHCState = () => {
-
-    const {familyHCfetch, isLoading, error } = useFetchAllFamilyHC();
-    const [familyHCFamily, setFamilyHCFamily] = useState([]);
-
-    useEffect(() => {
-        if(familyHCfetch){
-            setFamilyHCFamily(familyHCfetch)
-=======
 import { FamilyHCController } from "../../../sdk/HappyChild/FamilyHC/controller";
 
 export const FamilyHCState = () => {
@@ -21,7 +9,6 @@ export const FamilyHCState = () => {
     useEffect(() => {
         if(FamilyHCfetch){
             setFamilyHCFamily(FamilyHCfetch)
->>>>>>> 934d3c805e1f7b73812cc389d1c8b50ea2f74c74
         }
     }, [familyHCfetch])
     
