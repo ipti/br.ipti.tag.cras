8479626218828282e3b07c6140146709ddef6afa
Jonny <62815240+jonnypaulino@users.noreply.github.com>
2026-05-29 17:49:12 -0300

    Change baseURL for Axios instance

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,7 +4,7 @@ import { getToken } from "./localstorage";
 // require('dotenv').config();
 
 const http = axios.create({
-  baseURL: "https://api-cras.tag.ong.br/",
+  baseURL: "https://br-ong-tag-cras.azurewebsites.net/",
   // baseURL: "http://localhost:3001/",
 });
 
